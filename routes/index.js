35d7443f2181ff190bdb1af63540cdbe7577890f
mmonereo <mauro.monereo@gmail.com>
2021-11-22 13:18:56 +0100

    added route structure

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,5 @@ module.exports = app => {
     app.use("/", require("./auth.routes")); 
     // app.use("/", require("./user.routes"));
 	app.use("/sunsets", require("./sunset.routes"));
-	app.use("/sunrise", require("./sunrise.routes"));
- 
+	app.use("/sunrises", require("./sunrise.routes"));
   }
--- routes/sunset.routes.js
+++ routes/sunset.routes.js
@@ -2,7 +2,15 @@ const router = require("express").Router()
 const User = require("../models/User.model")
 const Sun = require("../models/Sun.model")
 
-
+router.get('/', (req, res, next) => {
+	Sun.find({category: "sunset"})
+		.then(sunsets => {
+			res.render('/sun/every-sun', {sunsets})
+		})
+		.catch(err => {
+			console.log(err)
+		});
+})
 
 
 module.exports = router
