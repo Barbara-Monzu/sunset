a50a512f674134146482a9282e4dfa34d3312822
mmonereo <mauro.monereo@gmail.com>
2021-11-25 15:02:39 +0100

    remove node_modules

--- public/js/profile-favorites.js
+++ public/js/profile-favorites.js
@@ -5,18 +5,11 @@ const deleteFav = document.querySelectorAll('#removeFavorite').forEach(elem => e
 	const category = elem.childNodes[3].value;
 	const id = elem.childNodes[1].value;
 	deleteFavorite(id, category)
+	const firstParent = e.currentTarget.parentNode
+	firstParent.parentNode.remove()
 }
 ))
 
-
-function addFavorite(sunId) {
-	const axiosApp = axios.create({ baseURL: `http://localhost:3000/suns/${category}/list/${sunId}/add-favorite` })
-
-	axiosApp.post()
-		.then(response => {
-			console.log("added favorite")
-		})
-}
 
 function deleteFavorite(sunId, category) {
 	const axiosApp = axios.create({ baseURL: `http://localhost:3000/suns/${category}/list/${sunId}/delete-favorite` })
